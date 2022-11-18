067137aa9ed7fbdaa2fcd86e1905994f1a4dfc61
Isabella <isalopci@gmail.com>
2022-11-17 23:09:36 -0500

    Create a new to method to obtain the authors name from the docs dictionary

--- usefulDataGetter.py
+++ usefulDataGetter.py
@@ -8,6 +8,11 @@ class usefulDataGetter:
     def extractTitle(raw_doc:dict):
         return raw_doc.get('title')
 
+    @staticmethod
+    def extractAuthors(raw_doc:dict):
+        authors = raw_doc.get('author_name')
+        return ', '.join(authors)
+
     def fetchRawDocs(self):
         raw_dictionary = self.books_response
         return raw_dictionary.get('docs')
