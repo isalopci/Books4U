6d6b167624b19f48238528c4f28e9e8e1956734d
Isabella <isalopci@gmail.com>
2022-11-17 23:08:21 -0500

    Created a new method to extract the title of the book from the doc dictionary

--- usefulDataGetter.py
+++ usefulDataGetter.py
@@ -4,6 +4,10 @@ class usefulDataGetter:
     def __init__(self, title:str = None, author:str = None) -> None:
         self.books_response = requestApi(title,author).fetchBooks()
 
+    @staticmethod
+    def extractTitle(raw_doc:dict):
+        return raw_doc.get('title')
+
     def fetchRawDocs(self):
         raw_dictionary = self.books_response
         return raw_dictionary.get('docs')
