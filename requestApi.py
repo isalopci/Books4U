97d9b256912b736a9b3557b4f51fbd47f29062ee
Isabella <isalopci@gmail.com>
2022-11-18 03:13:59 -0500

    Added comments

--- requestApi.py
+++ requestApi.py
@@ -1,14 +1,18 @@
 import re
 import requests
 
+
+# This class is to make all requests to the API
 class requestApi:
     def __init__(self, title:str = None, author:str = None) -> None:
         self.title = self.formatString(title) if title else title 
         self.author = self.formatString(author) if author else author
 
+    # Method to forma the inserted string (author or book) 
     def formatString(self, name:str) -> str:
         return re.sub(r'\s+','%20',name)
-        
+    
+
     def buildUrl(self) -> str:
 
         base_url = 'http://openlibrary.org/search.json'
@@ -27,7 +31,7 @@ class requestApi:
             raise Exception('An image size must be entered (S, M or L)')
         return f'https://covers.openlibrary.org/b/id/{id}-{size}.jpg'
 
-
+    # Fetch book information and return it as json format
     def fetchBooks(self):
         url = self.buildUrl()
         response = requests.get(url)
