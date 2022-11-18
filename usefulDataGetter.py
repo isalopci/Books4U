2eba0a70b0f3f2358ea9f320d9ff7837a2de95c1
Isabella <isalopci@gmail.com>
2022-11-17 23:00:41 -0500

    Deleted data2json method since it is not necessary anymore

--- usefulDataGetter.py
+++ usefulDataGetter.py
@@ -1,13 +1,8 @@
-import json
 from requestApi import requestApi
 
 class usefulDataGetter:
     def __init__(self, title:str = None, author:str = None) -> None:
         self.books_response = requestApi(title,author).fetchBooks()
-
-    def data2json(self) -> dict:
-        json_response = json.loads(self.books_response)
-        return json_response
 
     def extractInfo(self, raw_docs:dict):
         raw_docs
