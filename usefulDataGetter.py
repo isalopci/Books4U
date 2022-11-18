404e404e6d12e559d506388322435e03e14118f0
Isabella <isalopci@gmail.com>
2022-11-17 23:07:27 -0500

    Deleted extractInfo method and created a new method fetchRawDocs to get the doc dictionary with all the info of a book from the response of fetchBooks

--- usefulDataGetter.py
+++ usefulDataGetter.py
@@ -4,14 +4,6 @@ class usefulDataGetter:
     def __init__(self, title:str = None, author:str = None) -> None:
         self.books_response = requestApi(title,author).fetchBooks()
 
-    def extractInfo(self, raw_docs:dict):
-        raw_docs
-        pass
-
-    def main(self):
-        raw_dictionary = self.data2json()
-        raw_docs = raw_dictionary.get('docs')
-
-        pass
-
-
+    def fetchRawDocs(self):
+        raw_dictionary = self.books_response
+        return raw_dictionary.get('docs')
